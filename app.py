21c7e35a8a5ab3ea00ea0c608d5bdafba8b11aa6
atharvbakre <atharvbakre4567@gmail.com>
2022-11-01 00:58:24 +0530

    Test

--- app.py
+++ app.py
@@ -13,21 +13,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 app = Flask(__name__)
 
-def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
-
-@app.route("/site-map")
-def site_map():
-    links = []
-    for rule in app.url_map.iter_rules():
-        # Filter out rules we can't navigate to in a browser
-        # and rules that require parameters
-        if "GET" in rule.methods and has_no_empty_params(rule):
-            url = url_for(rule.endpoint, **(rule.defaults or {}))
-            links.append((url, rule.endpoint))
-
 '''' BOL '''
 @app.route("/bol/<ISBN>")
 def getBookFromISBN_BOL(ISBN):
