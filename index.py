8b68ee873bdeb275c042961ccb195a05a2bd2632
atharvbakre <atharvbakre4567@gmail.com>
2022-10-31 23:39:02 +0530

    Test

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 # from types import NoneType
 from currency_converter import CurrencyConverter
@@ -11,6 +11,21 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 app = Flask(__name__)
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
 
 '''' BOL '''
 @app.route("/bol/<ISBN>")
