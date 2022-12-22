95082a440caf71a64d86aea3a02e9842cd64a830
atharvbakre <atharvbakre4567@gmail.com>
2022-12-22 22:03:03 +0530

    New Updates

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
-# from types import NoneType
-# from currency_converter import CurrencyConverter
+from currency_converter import CurrencyConverter
 from bs4 import BeautifulSoup
 import requests
 # from waitress import serve
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
 
 app = Flask(__name__)
 CORS(app)
@@ -25,7 +19,7 @@ def getBookFromISBN_BOL(ISBN):
 
     for isbn in ISBNs:
 
-        bolURL = "https://www.bol.com/nl/nl/s/?searchtext=" + str(isbn)
+        bolURL = "https://www.bol.com/nl/nl/s/?sort=price0&searchtext=" + str(isbn)
         page = requests.get(bolURL)
         soup = BeautifulSoup(page.text, "lxml")
         try:
@@ -33,11 +27,23 @@ def getBookFromISBN_BOL(ISBN):
                 "title":"",
                 "price":"",
                 "imgURL":"",
-                "link": bolURL
+                "link": bolURL,
+                "binding": ""
             }
-            content = soup.find('div', class_='product-item__content');
-            bolTitle = content.find('a', class_='product-title').get_text()
-            bolPriceSections = content.find_all('div', class_='product-prices');
+            content = soup.find('div', class_='product-item__content')
+            bolTitleSection = content.find('a', class_='product-title')
+            bolTitle = bolTitleSection.get_text()
+            
+            bolBookPage = bolTitleSection["href"]
+            bolBook = requests.get('https://www.bol.com' + bolBookPage)
+            bolPage = BeautifulSoup(bolBook.text, 'lxml')
+            bolImageContainer = bolPage.find('wsp-image-zoom').div.img
+            try:
+                bolImage = bolImageContainer['data-zoom-image-url']
+            except:
+                bolImage = bolImageContainer['src']
+            
+            bolPriceSections = content.find_all('div', class_='product-prices')
             if(len(bolPriceSections) > 1):
                 bolUsedBook = content.find_all('div', class_='product-prices')[1]
                 bolURL = "https://www.bol.com" + bolUsedBook.find_all('a')[1]["href"]
@@ -45,17 +51,17 @@ def getBookFromISBN_BOL(ISBN):
                 bolPrice = bolPriceSplit[-1].replace(',', '.')
                 response["link"] = bolURL
             else:
-                bolPriceSection = bolPriceSections[0].find('meta', attrs={"itemprop": "price"});
+                bolPriceSection = bolPriceSections[0].find('meta', attrs={"itemprop": "price"})
                 if(bolPriceSection):
-                    bolPrice = bolPriceSection["content"];
+                    bolPrice = bolPriceSection["content"]
                 else:
-                    bolPrice = " ".join(bolPriceSections[0].get_text().split());
-                
-            bolImage = soup.find('div', class_='product-item__image').find('img')["src"]
+                    bolPrice = " ".join(bolPriceSections[0].get_text().split())
             
+            bolBinding = soup.find('ul', class_='product-small-specs').findAll('li')[1].span.text
             response["title"] = bolTitle
             response["price"] = bolPrice
             response["imgURL"] = bolImage
+            response["binding"] = bolBinding
             responses[isbn] = response
         except Exception as e:
             responses[isbn] = {"error": "Book not found on bol.com"}
@@ -69,7 +75,7 @@ def getBookFromISBN_BOL(ISBN):
 '''' BOEKWINKELTJES '''
 @app.route("/boekwinkeltjes/<ISBN>")
 def getBookFromISBN_BOEK(ISBN):
-    boekwinkeltjesURL = 'https://www.boekwinkeltjes.nl/s/?q=' + str(ISBN)
+    boekwinkeltjesURL = 'https://www.boekwinkeltjes.nl/s/?sort=prijs&q=' + str(ISBN)
     page = requests.get(boekwinkeltjesURL)
     soup = BeautifulSoup(page.text, 'lxml')
     try:
@@ -94,7 +100,7 @@ def getBookFromISBN_BOEK(ISBN):
 '''' DESLEGTE '''
 @app.route("/deslegte/<ISBN>")
 def getBookFromISBN_DES(ISBN):
-    deslegateURL = "https://www.deslegte.com/boeken/?q=" + str(ISBN)
+    deslegateURL = "https://www.deslegte.com/boeken/?sc=price&so=asc&q=" + str(ISBN)
     page = requests.get(deslegateURL)
     soup = BeautifulSoup(page.text, 'lxml')
     try:
@@ -171,13 +177,12 @@ def getBookFromISBN_AMAZON(ISBN):
             "imgURL": "https://m.media-amazon.com/images/I/51qCkoJsS6L._SY487_BO1,204,203,200_.jpg",
             "link": "https://amzn.to/3GrLywv"
         }
-    return "Book not found on abebooks.com"
+    return "Book not found on amazon.com"
 
 @app.route("/")
 def index():
     return "<h1>Hello!</h1>"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
-
