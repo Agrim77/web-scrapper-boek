2a379465b224e6fbc462d8e64da9fd36589dafe7
Agrim Sharma <sharma.agrim77@gmail.com>
2023-06-30 01:19:22 +0530

    BOL API Integration Implemented

--- app.py
+++ app.py
@@ -13,63 +13,83 @@ CORS(app)
 '''' BOL '''
 @app.route("/bol/<ISBN>")
 def getBookFromISBN_BOL(ISBN):
-    
     ISBNs = ISBN.split(',')
-
     responses = {}
-
+    
+    # Authentication Process
+    client_id = "c866b744-6084-4afc-b575-137d176c9be6"
+    client_secret = "a5MZZ10Yg!XFjCQDmecxU1hmtfwIQKJKZebB3WXoO?4CAsPyRVfs(zQ(pUON)q(8"
+    message = f"{client_id}:{client_secret}"
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    encoded_message = base64_bytes.decode('ascii')
+    authorization_header = f"Basic {encoded_message}"
+    
     for isbn in ISBNs:
-
-        bolURL = "https://www.bol.com/nl/nl/s/?sort=price0&searchtext=" + str(isbn)
-        page = requests.get(bolURL)
-        soup = BeautifulSoup(page.text, "lxml")
+        singleBookResponse = {
+            "title":"",
+            "price":"",
+            "imgURL":"",
+            "link": "",
+            "binding": "",
+            "language": ""
+        }
+        
+        bolURL = "https://api.bol.com/catalog/v4/search"
+        bolAuthenticateURL = "https://login.bol.com/token?grant_type=client_credentials"
+        headers = {"Authorization": authorization_header}
+        
         try:
-            response = {
-                "title":"",
-                "price":"",
-                "imgURL":"",
-                "link": bolURL,
-                "binding": ""
-            }
-            content = soup.find('div', class_='product-item__content')
-            bolTitleSection = content.find('a', class_='product-title')
-            bolTitle = bolTitleSection.get_text()
+            response = requests.post(bolAuthenticateURL, headers=headers)
+            print(response)
             
-            bolBookPage = bolTitleSection["href"]
-            bolBook = requests.get('https://www.bol.com' + bolBookPage)
-            bolPage = BeautifulSoup(bolBook.text, 'lxml')
-            bolImageContainer = bolPage.find('wsp-image-zoom').div.img
-            try:
-                bolImage = bolImageContainer['data-zoom-image-url']
-            except:
-                bolImage = bolImageContainer['src']
-            
-            bolPriceSections = content.find_all('div', class_='product-prices')
-            if(len(bolPriceSections) > 1):
-                bolUsedBook = content.find_all('div', class_='product-prices')[1]
-                bolURL = "https://www.bol.com" + bolUsedBook.find_all('a')[1]["href"]
-                bolPriceSplit = bolUsedBook.get_text().split()
-                bolPrice = bolPriceSplit[-1].replace(',', '.')
-                response["link"] = bolURL
-            else:
-                bolPriceSection = bolPriceSections[0].find('meta', attrs={"itemprop": "price"})
-                if(bolPriceSection):
-                    bolPrice = bolPriceSection["content"]
-                else:
-                    bolPrice = " ".join(bolPriceSections[0].get_text().split())
-            
-            bolBinding = soup.find('ul', class_='product-small-specs').findAll('li')[1].span.text
-            response["title"] = bolTitle
-            response["price"] = bolPrice
-            response["imgURL"] = bolImage
-            response["binding"] = bolBinding
-            responses[isbn] = response
+            if response.status_code == 200:
+                access_token = response.json()["access_token"]
+                bearer_token = f"Bearer {access_token}"
+                headers = {"Authorization": bearer_token}
+                
+                try:
+                    response = requests.get(bolURL, headers=headers, params={
+                                    "includeattributes": "true", 
+                                    "q": '9781611805901', 
+                                    "limit": 1})
+                    
+                    if response.status_code == 200:
+                        data = response.json()
+                        singleData = data["products"][0]
+                        
+                        for url in singleData['urls']:
+                            if url["key"] == "DESKTOP":
+                                tempUrl = url["value"]
+                                
+                        for images in singleData['images']:
+                            if images["key"] == "XL":
+                                tempImage = images["url"]
+                                
+                        for contents in singleData['attributeGroups']:
+                            if contents["title"] == "Inhoud":
+                                for content in contents["attributes"]:
+                                    if content["key"] == "Binding":
+                                        tempBinding = content["value"]
+                                    if content["key"] == "Language":
+                                        tempLanguage = content["value"]
+                                
+                        singleBookResponse["title"] = singleData["title"]
+                        singleBookResponse["price"] = singleData['offerData']['offers'][0]['price']
+                        singleBookResponse["imgURL"] = tempImage
+                        singleBookResponse["binding"] = tempBinding
+                        singleBookResponse["language"] = tempLanguage
+                        singleBookResponse["link"] = tempUrl
+                        responses[isbn] = singleBookResponse
+                        
+                except Exception as e:
+                    raise e
         except Exception as e:
-            responses[isbn] = {"error": "Book not found on bol.com"}
-
+                    raise e
+    
     if len(ISBNs) == 1:
         return responses[ISBN]
-        
+    
     return responses
 
 
@@ -191,6 +211,75 @@ def getBookFromISBN_AMAZON(ISBN):
             "link": "https://amzn.to/3GrLywv"
         }
     return "Book not found on amazon.com"
+
+
+'''' BOL - OLD CODE '''
+# @app.route("/bol/<ISBN>")
+# def getBookFromISBN_BOL(ISBN):
+
+#     ISBNs = ISBN.split(',')
+
+#     responses = {}
+
+#     for isbn in ISBNs:
+
+#         bolURL = "https://www.bol.com/nl/nl/s/?sort=price0&searchtext=" + \
+#             str(isbn)
+#         page = requests.get(bolURL)
+#         soup = BeautifulSoup(page.text, "lxml")
+#         try:
+#             response = {
+#                 "title": "",
+#                 "price": "",
+#                 "imgURL": "",
+#                 "link": bolURL,
+#                 "binding": ""
+#             }
+#             content = soup.find('div', class_='product-item__content')
+#             bolTitleSection = content.find('a', class_='product-title')
+#             bolTitle = bolTitleSection.get_text()
+
+#             bolBookPage = bolTitleSection["href"]
+#             bolBook = requests.get('https://www.bol.com' + bolBookPage)
+#             bolPage = BeautifulSoup(bolBook.text, 'lxml')
+#             bolImageContainer = bolPage.find('wsp-image-zoom').div.img
+#             try:
+#                 bolImage = bolImageContainer['data-zoom-image-url']
+#             except:
+#                 bolImage = bolImageContainer['src']
+
+#             bolPriceSections = content.find_all('div', class_='product-prices')
+#             if (len(bolPriceSections) > 1):
+#                 bolUsedBook = content.find_all(
+#                     'div', class_='product-prices')[1]
+#                 bolURL = "https://www.bol.com" + \
+#                     bolUsedBook.find_all('a')[1]["href"]
+#                 bolPriceSplit = bolUsedBook.get_text().split()
+#                 bolPrice = bolPriceSplit[-1].replace(',', '.')
+#                 response["link"] = bolURL
+#             else:
+#                 bolPriceSection = bolPriceSections[0].find(
+#                     'meta', attrs={"itemprop": "price"})
+#                 if (bolPriceSection):
+#                     bolPrice = bolPriceSection["content"]
+#                 else:
+#                     bolPrice = " ".join(bolPriceSections[0].get_text().split())
+
+#             bolBinding = soup.find(
+#                 'ul', class_='product-small-specs').findAll('li')[1].span.text
+#             response["title"] = bolTitle
+#             response["price"] = bolPrice
+#             response["imgURL"] = bolImage
+#             response["binding"] = bolBinding
+#             responses[isbn] = response
+#         except Exception as e:
+#             responses[isbn] = {"error": "Book not found on bol.com"}
+
+#     if len(ISBNs) == 1:
+#         return responses[ISBN]
+
+#     return responses
+
 
 @app.route("/")
 def index():
